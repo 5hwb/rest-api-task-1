39ef3eb0bddaa70708ba96bdcd4ccce05e873850
Perry Hartono <phar2413@uni.sydney.edu.au>
2021-05-22 22:05:12 +1000

    Moved the location test cases before the spaceship test cases (since spaceships now depend on locations to exist)

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -6,6 +6,155 @@ describe('REST API Test Cases', () => {
 
   // ===============================================
   // ===============================================
+  // Location creating
+  // ===============================================
+  // ===============================================
+
+  it('should create a new location', async () => {
+    const res = await request(app)
+      .put('/locations/create')
+      .send({"id": 10, "cityName": "Skydust", "planetName": "Pluto", "capacity": 2});
+      
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('location_was_created');
+    expect(res.body.location_was_created).toEqual(true);
+  });
+
+  //////////////////////////////////////////////////
+  // Error checking for create operations
+  //////////////////////////////////////////////////
+
+  it('should give an error if location ID is missing', async () => {
+    const res = await request(app)
+      .put('/locations/create')
+      .send({"cityName": "Skydust", "planetName": "Pluto", "capacity": 2});
+      
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toHaveProperty('location_was_created');
+    expect(res.body.location_was_created).toEqual(false);
+    expect(res.body).toHaveProperty("error");
+    expect(res.body.error).toEqual("Invalid parameters");
+  });
+
+  it('should give an error if city name is missing', async () => {
+    const res = await request(app)
+      .put('/locations/create')
+      .send({"id": 10, "planetName": "Pluto", "capacity": 2});
+      
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toHaveProperty('location_was_created');
+    expect(res.body.location_was_created).toEqual(false);
+    expect(res.body).toHaveProperty("error");
+    expect(res.body.error).toEqual("Invalid parameters");
+  });
+
+  it('should give an error if planet name is missing', async () => {
+    const res = await request(app)
+      .put('/locations/create')
+      .send({"id": 10, "cityName": "Skydust", "capacity": 2});
+      
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toHaveProperty('location_was_created');
+    expect(res.body.location_was_created).toEqual(false);
+    expect(res.body).toHaveProperty("error");
+    expect(res.body.error).toEqual("Invalid parameters");
+  });
+
+  it('should give an error if capacity info is missing', async () => {
+    const res = await request(app)
+      .put('/locations/create')
+      .send({"id": 10, "cityName": "Skydust", "planetName": "Pluto"});
+      
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toHaveProperty('location_was_created');
+    expect(res.body.location_was_created).toEqual(false);
+    expect(res.body).toHaveProperty("error");
+    expect(res.body.error).toEqual("Invalid parameters");
+  });
+
+  // ===============================================
+  // ===============================================
+  // Location reading
+  // ===============================================
+  // ===============================================
+
+  it('should be able to read the new location', async () => {
+    const res = await request(app)
+      .get('/locations/10');
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('data');
+    expect(res.body.data).toHaveProperty('id');
+    expect(res.body.data.id).toEqual(10);
+    expect(res.body.data).toHaveProperty('cityName');
+    expect(res.body.data.cityName).toEqual("Skydust");
+    expect(res.body.data).toHaveProperty('planetName');
+    expect(res.body.data.planetName).toEqual("Pluto");
+    expect(res.body.data).toHaveProperty('capacity');
+    expect(res.body.data.capacity).toEqual(2);
+  });
+
+  //////////////////////////////////////////////////
+  // Error checking for read operations
+  //////////////////////////////////////////////////
+
+  it('should give an error when reading a nonexistent location', async () => {
+    const res = await request(app)
+      .get('/locations/11');
+
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toHaveProperty("error");
+    expect(res.body.error).toEqual("Invalid ID");
+  });
+
+  it('should give an error when reading with invalid URL parameters', async () => {
+    const res = await request(app)
+      .get('/locations/eqeqeq');
+
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toHaveProperty("error");
+    expect(res.body.error).toEqual("Invalid ID");
+  });
+
+  // ===============================================
+  // ===============================================
+  // Location deleting 
+  // ===============================================
+  // ===============================================
+
+  it('should be able to delete the location', async () => {
+    const res = await request(app)
+      .delete('/locations/delete/10');
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty('location_was_deleted');
+    expect(res.body.location_was_deleted).toEqual(true);
+  });
+
+  //////////////////////////////////////////////////
+  // Error checking for delete operations
+  //////////////////////////////////////////////////
+
+  it('should give an error when deleting a nonexistent location', async () => {
+    const res = await request(app)
+      .delete('/locations/delete/11');
+
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toHaveProperty("error");
+    expect(res.body.error).toEqual("Invalid ID");
+  });
+
+  it('should give an error when deleting with invalid URL parameters', async () => {
+    const res = await request(app)
+      .delete('/locations/delete/qeqqeqe');
+
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toHaveProperty("error");
+    expect(res.body.error).toEqual("Invalid ID");
+  });
+
+  // ===============================================
+  // ===============================================
   // Spaceship creating
   // ===============================================
   // ===============================================
@@ -211,155 +360,6 @@ describe('REST API Test Cases', () => {
   it('should give an error when deleting with invalid URL parameters', async () => {
     const res = await request(app)
       .delete('/spaceships/delete/qeqqeqe');
-
-    expect(res.statusCode).toEqual(400);
-    expect(res.body).toHaveProperty("error");
-    expect(res.body.error).toEqual("Invalid ID");
-  });
-
-  // ===============================================
-  // ===============================================
-  // Location creating
-  // ===============================================
-  // ===============================================
-
-  it('should create a new location', async () => {
-    const res = await request(app)
-      .put('/locations/create')
-      .send({"id": 10, "cityName": "Skydust", "planetName": "Pluto", "capacity": 2});
-      
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty('location_was_created');
-    expect(res.body.location_was_created).toEqual(true);
-  });
-
-  //////////////////////////////////////////////////
-  // Error checking for create operations
-  //////////////////////////////////////////////////
-
-  it('should give an error if location ID is missing', async () => {
-    const res = await request(app)
-      .put('/locations/create')
-      .send({"cityName": "Skydust", "planetName": "Pluto", "capacity": 2});
-      
-    expect(res.statusCode).toEqual(400);
-    expect(res.body).toHaveProperty('location_was_created');
-    expect(res.body.location_was_created).toEqual(false);
-    expect(res.body).toHaveProperty("error");
-    expect(res.body.error).toEqual("Invalid parameters");
-  });
-
-  it('should give an error if city name is missing', async () => {
-    const res = await request(app)
-      .put('/locations/create')
-      .send({"id": 10, "planetName": "Pluto", "capacity": 2});
-      
-    expect(res.statusCode).toEqual(400);
-    expect(res.body).toHaveProperty('location_was_created');
-    expect(res.body.location_was_created).toEqual(false);
-    expect(res.body).toHaveProperty("error");
-    expect(res.body.error).toEqual("Invalid parameters");
-  });
-
-  it('should give an error if planet name is missing', async () => {
-    const res = await request(app)
-      .put('/locations/create')
-      .send({"id": 10, "cityName": "Skydust", "capacity": 2});
-      
-    expect(res.statusCode).toEqual(400);
-    expect(res.body).toHaveProperty('location_was_created');
-    expect(res.body.location_was_created).toEqual(false);
-    expect(res.body).toHaveProperty("error");
-    expect(res.body.error).toEqual("Invalid parameters");
-  });
-
-  it('should give an error if capacity info is missing', async () => {
-    const res = await request(app)
-      .put('/locations/create')
-      .send({"id": 10, "cityName": "Skydust", "planetName": "Pluto"});
-      
-    expect(res.statusCode).toEqual(400);
-    expect(res.body).toHaveProperty('location_was_created');
-    expect(res.body.location_was_created).toEqual(false);
-    expect(res.body).toHaveProperty("error");
-    expect(res.body.error).toEqual("Invalid parameters");
-  });
-
-  // ===============================================
-  // ===============================================
-  // Location reading
-  // ===============================================
-  // ===============================================
-
-  it('should be able to read the new location', async () => {
-    const res = await request(app)
-      .get('/locations/10');
-
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty('data');
-    expect(res.body.data).toHaveProperty('id');
-    expect(res.body.data.id).toEqual(10);
-    expect(res.body.data).toHaveProperty('cityName');
-    expect(res.body.data.cityName).toEqual("Skydust");
-    expect(res.body.data).toHaveProperty('planetName');
-    expect(res.body.data.planetName).toEqual("Pluto");
-    expect(res.body.data).toHaveProperty('capacity');
-    expect(res.body.data.capacity).toEqual(2);
-  });
-
-  //////////////////////////////////////////////////
-  // Error checking for read operations
-  //////////////////////////////////////////////////
-
-  it('should give an error when reading a nonexistent location', async () => {
-    const res = await request(app)
-      .get('/locations/11');
-
-    expect(res.statusCode).toEqual(400);
-    expect(res.body).toHaveProperty("error");
-    expect(res.body.error).toEqual("Invalid ID");
-  });
-
-  it('should give an error when reading with invalid URL parameters', async () => {
-    const res = await request(app)
-      .get('/locations/eqeqeq');
-
-    expect(res.statusCode).toEqual(400);
-    expect(res.body).toHaveProperty("error");
-    expect(res.body.error).toEqual("Invalid ID");
-  });
-
-  // ===============================================
-  // ===============================================
-  // Location deleting 
-  // ===============================================
-  // ===============================================
-
-  it('should be able to delete the location', async () => {
-    const res = await request(app)
-      .delete('/locations/delete/10');
-
-    expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty('location_was_deleted');
-    expect(res.body.location_was_deleted).toEqual(true);
-  });
-
-  //////////////////////////////////////////////////
-  // Error checking for delete operations
-  //////////////////////////////////////////////////
-
-  it('should give an error when deleting a nonexistent location', async () => {
-    const res = await request(app)
-      .delete('/locations/delete/11');
-
-    expect(res.statusCode).toEqual(400);
-    expect(res.body).toHaveProperty("error");
-    expect(res.body.error).toEqual("Invalid ID");
-  });
-
-  it('should give an error when deleting with invalid URL parameters', async () => {
-    const res = await request(app)
-      .delete('/locations/delete/qeqqeqe');
 
     expect(res.statusCode).toEqual(400);
     expect(res.body).toHaveProperty("error");
