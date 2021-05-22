bb32f9383c03ba95f091052cc5454f403e42cfa1
Perry Hartono <phar2413@uni.sydney.edu.au>
2021-05-22 23:04:07 +1000

    Add 2 methods to Location model to add incoming spaceships and remove outgoing spaceships

--- api/models/locationModel.ts
+++ api/models/locationModel.ts
@@ -1,5 +1,7 @@
 'use strict';
 
+import { Spaceship } from "./spaceshipModel";
+
 /**
  * Models relating to location
  */
@@ -8,10 +10,16 @@
  * Location data model
  */
 export class Location {
+  // Basic location info
   id: number;
   cityName: string;
   planetName: string;
-  capacity: number;
+
+  // Maximum number of spaceships able to park at this location
+  readonly capacity: number;
+
+  // Registry of spaceships stationed at this location
+  spaceshipRegistry: Map<number, Spaceship>;
 
   /**
    * Create a new Location instance.
@@ -21,6 +29,7 @@ export class Location {
     this.cityName = cityName;
     this.planetName = planetName;
     this.capacity = capacity;
+    this.spaceshipRegistry = new Map();
   }
 
   /**
@@ -29,5 +38,33 @@ export class Location {
    */
   toString(): string {
     return "id: " + this.id + ", cityName: " + this.cityName + ", planetName: " + this.planetName + ", capacity: " + this.capacity;
+  }
+
+  /**
+   * Add the given spaceship to the location registry.
+   * @param spaceship Spaceship to add to registry
+   * @returns True if spaceship was added, false otherwise
+   */
+  addIncomingSpaceship(spaceship: Spaceship): boolean {
+    if (spaceship === undefined 
+      || this.spaceshipRegistry.size + 1 > this.capacity) {
+      return false;
+    }
+    this.spaceshipRegistry.set(spaceship.id, spaceship);
+    return true;
+  }
+
+  /**
+   * Remove the given spaceship from the location registry.
+   * @param spaceship Spaceship to remove from registry
+   * @returns True if spaceship was removed, false otherwise
+   */
+  removeOutgoingSpaceship(spaceship: Spaceship): boolean {
+    if (spaceship === undefined || !this.spaceshipRegistry.has(spaceship.id) 
+      || this.spaceshipRegistry.size - 1 < 0) {
+      return false;
+    }
+    this.spaceshipRegistry.delete(spaceship.id);
+    return true;
   }
 }
