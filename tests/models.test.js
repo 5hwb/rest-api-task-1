31d31f26452acd85f59f037ec4e934c102a292a5
Perry Hartono <phar2413@uni.sydney.edu.au>
2021-05-23 00:07:30 +1000

    Add more test cases for Location

--- tests/models.test.js
+++ tests/models.test.js
@@ -18,7 +18,10 @@ describe('Model Test Cases', () => {
     expect(location2.spaceshipRegistry.size).toEqual(0);
   });
 
-  // Unit tests
+  //////////////////////////////////////////////////
+  // Unit tests for addIncomingSpaceship()
+  //////////////////////////////////////////////////
+
   it('should add an incoming spaceship', async () => {
     let dummyLoc = new Location(12, "A City", "Mars", 5);
     let locationUnderTest = new Location(13, "Some remote outpost", "Mars", 1);
@@ -38,6 +41,17 @@ describe('Model Test Cases', () => {
     // After adding a spaceship, it should not add another spaceship
     expect(locationUnderTest.addIncomingSpaceship(aSpaceship)).toBe(true);
     expect(locationUnderTest.addIncomingSpaceship(anotherSpaceship)).toBe(false);
+    expect(locationUnderTest.spaceshipRegistry.size).toEqual(1);
+  });
+
+  it('should not add the same spaceship again', async () => {
+    let dummyLoc = new Location(12, "A City", "Mars", 5);
+    let locationUnderTest = new Location(13, "Some remote outpost", "Mars", 1);
+    let aSpaceship = new Spaceship(3, "Old thingo", "1st Gen Skycar (2030)", dummyLoc);
+
+    expect(locationUnderTest.addIncomingSpaceship(aSpaceship)).toBe(true);
+    expect(locationUnderTest.addIncomingSpaceship(aSpaceship)).toBe(false);
+    expect(locationUnderTest.spaceshipRegistry.size).toEqual(1);
   });
 
   it('should not add undefined or null spaceships', async () => {
@@ -47,6 +61,44 @@ describe('Model Test Cases', () => {
 
     expect(locationUnderTest.addIncomingSpaceship(aNullSpaceship)).toBe(false);
     expect(locationUnderTest.addIncomingSpaceship(aNullSpaceship2)).toBe(false);
+  });
+
+  //////////////////////////////////////////////////
+  // Integration tests for removeOutgoingSpaceship()
+  //////////////////////////////////////////////////
+
+  it('should remove an outgoing spaceship', async () => {
+    let dummyLoc = new Location(12, "A City", "Mars", 5);
+    let locationUnderTest = new Location(13, "Some remote outpost", "Mars", 1);
+    let aSpaceship = new Spaceship(3, "Old thingo", "1st Gen Skycar (2030)", dummyLoc);
+
+    expect(locationUnderTest.addIncomingSpaceship(aSpaceship)).toBe(true);
+    expect(locationUnderTest.removeOutgoingSpaceship(aSpaceship)).toBe(true);
+    expect(locationUnderTest.spaceshipRegistry.size).toEqual(0);
+  });
+
+  it('should not remove the same spaceship again', async () => {
+    let dummyLoc = new Location(12, "A City", "Mars", 5);
+    let locationUnderTest = new Location(13, "Some remote outpost", "Mars", 1);
+    let aSpaceship = new Spaceship(3, "Old thingo", "1st Gen Skycar (2030)", dummyLoc);
+
+    expect(locationUnderTest.addIncomingSpaceship(aSpaceship)).toBe(true);
+    expect(locationUnderTest.removeOutgoingSpaceship(aSpaceship)).toBe(true);
+    expect(locationUnderTest.removeOutgoingSpaceship(aSpaceship)).toBe(false);
+    expect(locationUnderTest.spaceshipRegistry.size).toEqual(0);
+  });
+
+  it('should not remove undefined or null spaceships', async () => {
+    let dummyLoc = new Location(12, "A City", "Mars", 5);
+    let locationUnderTest = new Location(13, "Some remote outpost", "Mars", 1);
+    let aSpaceship = new Spaceship(3, "Old thingo", "1st Gen Skycar (2030)", dummyLoc);
+    let aNullSpaceship = undefined;
+    let aNullSpaceship2 = null;
+
+    expect(locationUnderTest.addIncomingSpaceship(aSpaceship)).toBe(true);
+    expect(locationUnderTest.removeOutgoingSpaceship(aNullSpaceship)).toBe(false);
+    expect(locationUnderTest.removeOutgoingSpaceship(aNullSpaceship2)).toBe(false);
+    expect(locationUnderTest.spaceshipRegistry.size).toEqual(1);
   });
 
 });
